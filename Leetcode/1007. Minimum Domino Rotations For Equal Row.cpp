965498ee4c898eb52bf2a63b7168ebc096459397
Ghribi Oussama <137878019+OussamaGhribi@users.noreply.github.com>
2025-05-03 20:07:09 +0200

    Create 1007. Minimum Domino Rotations For Equal Row.cpp

---
+++ Leetcode/1007. Minimum Domino Rotations For Equal Row.cpp
@@ -0,0 +1,23 @@
+// simplied version
+class Solution {
+public:
+    static int minDominoRotations(vector<int>& T, vector<int>& B) {
+        const int n=T.size();
+        bitset<8> X=255;// 8 bits are set
+        for(int i=0; i<n; i++){
+            X &=(1<<T[i])|(1<<B[i]);
+        }
+        if (X==0) return -1;
+        vector<int> cand;// contains at most 2
+        for(int i=1; i<=6; i++)
+            if (X[i]) cand.push_back(i);
+
+        int x=cand[0];// taking any of cand is OK
+        int swapT=0, swapB=0;
+        for(int i=0; i<n; i++){
+            if (T[i]!=x) swapT++;
+            if (B[i]!=x) swapB++;
+        }
+        return min(swapT, swapB);
+    }
+};
