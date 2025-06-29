cdfa2a48c81c23ac7752b74e02416d4d73eeed6e
Ghribi Oussama <137878019+OussamaGhribi@users.noreply.github.com>
2025-06-29 18:05:11 +0200

    Create 1498. Number of Subsequences That Satisfy the Given Sum Condition.cpp

---
+++ Leetcode/1498. Number of Subsequences That Satisfy the Given Sum Condition.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    const int MOD = 1e9 + 7;
+
+    int power(int base, int exp) {
+        long long result = 1;
+        long long b = base;
+        while (exp > 0) {
+            if (exp % 2 == 1) result = (result * b) % MOD;
+            b = (b * b) % MOD;
+            exp /= 2;
+        }
+        return result;
+    }
+
+    int numSubseq(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+
+        int res = 0;
+        int left = 0, right = nums.size() - 1;
+
+        while (left <= right) {
+            if (nums[left] + nums[right] <= target) {
+                res = (res + power(2, right - left)) % MOD;
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return res;
+    }
+};
