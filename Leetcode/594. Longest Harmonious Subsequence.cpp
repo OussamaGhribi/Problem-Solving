afa0b5990cac3b9b95130276c2057e3c23ff6982
Ghribi Oussama <137878019+OussamaGhribi@users.noreply.github.com>
2025-06-30 10:48:06 +0200

    Create 594. Longest Harmonious Subsequence.cpp

---
+++ Leetcode/594. Longest Harmonious Subsequence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+        unordered_map<int, int> freq;
+        int maxLen = 0;
+
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        for (auto& [num, count] : freq) {
+            if (freq.count(num + 1)) {
+                int combined = count + freq[num + 1];
+                maxLen = max(maxLen, combined);
+            }
+        }
+
+        return maxLen;
+    }
+};
